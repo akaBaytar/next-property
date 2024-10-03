94b7880ed3da6697024579ad21bff0bba2659d71
Burak Bilgili <burak.bilgili@icloud.com>
2024-10-03 15:47:07 +0300

    add mock property data and properties page layout

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,5 +1,21 @@
+import PROPERTIES from '@/mock/properties.json';
+
 const PropertiesPage = () => {
-  return <div>properties page</div>;
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {PROPERTIES.length === 0 ? (
+          <p>No properties found.</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {PROPERTIES.map((property) => (
+              <div key={property._id}>{property.name}</div>
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;
