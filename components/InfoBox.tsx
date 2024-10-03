1b0b1b9f2d333dbf70ac93d8c9cfce4b6c904188
Burak Bilgili <burak.bilgili@icloud.com>
2024-10-03 15:09:40 +0300

    add info boxes

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -1,5 +1,33 @@
-const InfoBox = () => {
-  return ( <div>info box</div> );
-}
- 
+import Link from 'next/link';
+
+type PropTypes = {
+  title: string;
+  text: string;
+  link: string;
+  buttonText: string;
+  className: string;
+  buttonClassName: string;
+};
+
+const InfoBox = ({
+  title,
+  text,
+  link,
+  buttonText,
+  className,
+  buttonClassName,
+}: PropTypes) => {
+  return (
+    <div className={`p-6 rounded-lg shadow-md ${className}`}>
+      <h2 className='text-2xl font-bold'>{title}</h2>
+      <p className='mt-2 mb-4 text-pretty'>{text}</p>
+      <Link
+        href={link}
+        className={`inline-block text-white rounded-md px-4 py-2 transition-colors duration-200 ${buttonClassName}`}>
+        {buttonText}
+      </Link>
+    </div>
+  );
+};
+
 export default InfoBox;
--- components/InfoBoxes.tsx
+++ components/InfoBoxes.tsx
@@ -1,5 +1,30 @@
+import InfoBox from './InfoBox';
+
 const InfoBoxes = () => {
-  return ( <div>info boxes</div> );
-}
- 
+  return (
+    <section>
+      <div className='container-xl lg:container m-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
+          <InfoBox
+            title='For Renters'
+            text='Find your dream rental property. Bookmark properties and contact owners.'
+            link='/properties'
+            buttonText='Browse Properties'
+            className='bg-gray-100'
+            buttonClassName='bg-black hover:bg-gray-900'
+          />
+          <InfoBox
+            title='For Property Owners'
+            text='List your properties and reach potential tenants. Rent as an airbnb or long term.'
+            link='/properties/add'
+            buttonText='Add Property'
+            className='bg-indigo-100'
+            buttonClassName='bg-indigo-500 hover:bg-indigo-600'
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default InfoBoxes;
