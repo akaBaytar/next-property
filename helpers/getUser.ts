30cafba206cbb802e4bc3a842bda9906e3da8367
Burak Bilgili <burak.bilgili@icloud.com>
2024-10-05 17:15:06 +0300

    get user helper function and update checkboxes accent color

---
+++ helpers/getUser.ts
@@ -0,0 +1,9 @@
+import { currentUser } from '@clerk/nextjs/server';
+
+export const getUser = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  return { user, userId: user.id };
+};
