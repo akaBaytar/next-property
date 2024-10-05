e6376b3a5268883bff0a46604bdf97fd4fa06c94
Burak Bilgili <burak.bilgili@icloud.com>
2024-10-05 14:05:39 +0300

    setup clerk webhook and user actions

---
+++ actions/user.action.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import prisma from '@/database';
+
+type CreateUser = {
+  id: string;
+  email: string;
+  username: string;
+  image: string;
+};
+
+export const createUser = async (user: CreateUser) => {
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.email,
+        image: user.image,
+        username: user.username,
+      },
+    });
+
+    return newUser;
+  } catch (error) {
+    console.error(error);
+  }
+};
