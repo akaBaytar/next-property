78ede9070c8527086f2dfb1af862b6bc949ad0f0
Burak Bilgili <burak.bilgili@icloud.com>
2024-10-09 18:10:56 +0300

    change context file to ts file, add screenshot of landing page and add license file

--- context/index.tsx
+++ context/index.tsx
@@ -5,9 +5,11 @@ import { useAuth } from '@clerk/nextjs';
 
 import { getNotification } from '@/actions/message.action';
 
-const GlobalContext = createContext();
+type Notification = { unreadCount: number };
 
-export const GlobalProvider = ({ children }) => {
+const GlobalContext = createContext<Notification>({ unreadCount: 0 });
+
+export const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   const [unreadCount, setUnreadCount] = useState(0);
 
   const { userId } = useAuth();
